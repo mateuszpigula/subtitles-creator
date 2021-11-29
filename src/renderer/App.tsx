cc1acc5b8a5011ad98eebde952efb2a2272ead77
Mateusz Pigula <pigula.mateusz@gmail.com>
2021-11-29 23:16:22 +0100

    Autoplay local video

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,22 +1,25 @@
-import React, { useEffect, useState } from 'react'
+import React, { useRef, useEffect } from "react";
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const videoRef = useRef<HTMLVideoElement>(null);
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setCount((c) => c + 1)
-    }, 1000)
+  const handleFileInput = (e) => {
+    if (!videoRef.current) {
+      return;
+    }
 
-    return () => clearInterval(timer)
-  }, [])
+    const file = e.target.files[0];
+    const fileURL = URL.createObjectURL(file);
+
+    videoRef.current.src = fileURL;
+  };
 
   return (
     <div className="padding">
-      <h1>Count {count}</h1>
-
       <div>Hello world!</div>
       <div>Fast full page reload from esbuild</div>
+      <input type="file" accept="video/*" onChange={handleFileInput} />
+      <video controls autoPlay ref={videoRef} width={500} />
     </div>
-  )
+  );
 }
